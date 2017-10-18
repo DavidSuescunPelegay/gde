20361119f5f3d80720874f814a52f0714e777f66
David Suescun Pelegay <dsuescun96@gmail.com>
2017-10-18 20:15:46 +0200

    v2.0.20161018

--- controladores/UsuariosC.php
+++ controladores/UsuariosC.php
@@ -3,112 +3,120 @@ require_once '/controladores/Controlador.php';
 require_once '/modelos/UsuariosM.php';
 require_once '/vistas/Vista.php';
 
-class UsuariosC extends Controlador{
-	private $modelo;
-	
-	public  function UsuariosC(){ //constructor del objeto
-		parent::Controlador(); //ejecuta el constructor padre
-		$this->modelo=new UsuariosM();
-	}
-	
-	public function validarUsuario($datos){
+class UsuariosC extends Controlador
+{
+    private $modelo;
+
+    public function UsuariosC()
+    { //constructor del objeto
+        parent::Controlador(); //ejecuta el constructor padre
+        $this->modelo = new UsuariosM();
+    }
+
+    public function validarUsuario($datos)
+    {
 // 		$login='';
 // 		$pass='';
 // 		extract($datos);
-		
-		$validacion=$this->modelo->validarUsuario($datos);
-		
+
+        $validacion = $this->modelo->validarUsuario($datos);
+
 // 		if($login=='javier' && $pass=='123'){
-		if( !empty($validacion) ){
-			$respuesta['valido']='S';
-			$respuesta['msj']='';
-		}else{
-			$respuesta['valido']='N';
-			$respuesta['msj']='Datos incorrectos';
-		}
-		$respuesta['datos']=$validacion;
-		return $respuesta;
-	}
-	
-	public function getVistaPrincipal(){
-		$vista=new Vista();
-		$vista->render($_SESSION['RAIZ'].'/vistas/Usuarios/UsuariosV.php');
-	}
-	
-	public function getVistaResultadosBusqueda($datos){
-		$filas=$this->modelo->findAll($datos);
-		//fb::log($filas);
-		$vista=new Vista();
-		$vista->render($_SESSION['RAIZ'].'/vistas/Usuarios/UsuariosBusquedaV.php',$filas);
-	}
-	
-	public function getDatosUsuario($datos){
-		$usuario=$this->modelo->getDatosUsuarioPorId($datos['id_Usuario']);
-		
-		$usuario['nombre']=utf8_encode($usuario['nombre']);
-		$usuario['apellido_1']=utf8_encode($usuario['apellido_1']);
-		$usuario['apellido_2']=utf8_encode($usuario['apellido_2']);
-		$usuario['login']=utf8_encode($usuario['login']);
-		
-		echo json_encode($usuario);
-	}
-	
-	public function guardarUsuario($datos){
-	
-		$respuesta['correcto']='S';
-		$respuesta['tipoError']='';
-		if($datos['id_Usuario']==0){
-			$respuesta['msj']=utf8_encode('Se ha guardado el nuevo usuario.');
-		}else{
-			$respuesta['msj']=utf8_encode('Se han guardado los cambios del usuario.');
-		}
-		//comprobar que no se repite el login
-		$filas=$this->modelo->findAll(array('login'=>$datos['login'],
-				'loginEXACTO'=>'S', 'activo'=>''));
-		if(!empty($filas) && $filas[0]['id_Usuario']!=$datos['id_Usuario']){
-			$respuesta['correcto']='N';
-			$respuesta['tipoError']='loginReptido';
-			$respuesta['msj']=utf8_encode('Login repetido, elija otro.');
-		}else{
-			if($datos['id_Usuario']==0){ //nuevo
-				$resul = $this->modelo->insertUsuario($datos);
-				if( $resul<1 ){
-					$respuesta['correcto']='N';
-					$respuesta['tipoError']='errorGuardar';
-					$respuesta['msj']=utf8_encode('Error al guardar.');
-						
-				}
-			}else{ //modificacion
-				if(isset($datos['pass']) && $datos['pass']!='') $datos['actualizarPass']='S';
-	
-				$resul = $this->modelo->updateUsuario($datos);
-				if( $resul!=1 ){
-					$respuesta['correcto']='N';
-					$respuesta['tipoError']='errorModificar';
-					$respuesta['msj']=utf8_encode('Error al modificar.');
-				
-				}
-			}
-		}
-	
-		echo json_encode($respuesta);
-	}
-	
-	public function activarDesactivar($datos){
-		$respuesta['correcto']='S';
-		$respuesta['tipoError']='';
-		if($datos['activo']=='S'){
-			$respuesta['msj']=utf8_encode('Se a activado el usuario.');
-		}else{
-			$respuesta['msj']=utf8_encode('Se desactivado el usuario.');
-		}
-		$resul = $this->modelo->activarDesactivarUsuario($datos);
-		if( $resul!=1 ){
-			$respuesta['correcto']='N';
-			$respuesta['tipoError']='errorModificar';
-			$respuesta['msj']=utf8_encode('Error al cambiar el estado del usuario.');
-		
-		}
-		echo json_encode($respuesta);
-	}
+        if (!empty($validacion)) {
+            $respuesta['valido'] = 'S';
+            $respuesta['msj'] = '';
+        } else {
+            $respuesta['valido'] = 'N';
+            $respuesta['msj'] = 'Datos incorrectos';
+        }
+        $respuesta['datos'] = $validacion;
+        return $respuesta;
+    }
+
+    public function getVistaPrincipal()
+    {
+        $vista = new Vista();
+        $vista->render($_SESSION['RAIZ'] . '/vistas/Usuarios/UsuariosV.php');
+    }
+
+    public function getVistaResultadosBusqueda($datos)
+    {
+        $filas = $this->modelo->findAll($datos);
+        //fb::log($filas);
+        $vista = new Vista();
+        $vista->render($_SESSION['RAIZ'] . '/vistas/Usuarios/UsuariosBusquedaV.php', $filas);
+    }
+
+    public function getDatosUsuario($datos)
+    {
+        $usuario = $this->modelo->getDatosUsuarioPorId($datos['id_Usuario']);
+
+        $usuario['nombre'] = utf8_encode($usuario['nombre']);
+        $usuario['apellido_1'] = utf8_encode($usuario['apellido_1']);
+        $usuario['apellido_2'] = utf8_encode($usuario['apellido_2']);
+        $usuario['login'] = utf8_encode($usuario['login']);
+
+        echo json_encode($usuario);
+    }
+
+    public function guardarUsuario($datos)
+    {
+
+        $respuesta['correcto'] = 'S';
+        $respuesta['tipoError'] = '';
+        if ($datos['id_Usuario'] == 0) {
+            $respuesta['msj'] = utf8_encode('Se ha guardado el nuevo usuario.');
+        } else {
+            $respuesta['msj'] = utf8_encode('Se han guardado los cambios del usuario.');
+        }
+        //comprobar que no se repite el login
+        $filas = $this->modelo->findAll(array('login' => $datos['login'],
+            'loginEXACTO' => 'S', 'activo' => ''));
+        if (!empty($filas) && $filas[0]['id_Usuario'] != $datos['id_Usuario']) {
+            $respuesta['correcto'] = 'N';
+            $respuesta['tipoError'] = 'loginReptido';
+            $respuesta['msj'] = utf8_encode('Login repetido, elija otro.');
+        } else {
+            if ($datos['id_Usuario'] == 0) { //nuevo
+                $resul = $this->modelo->insertUsuario($datos);
+                if ($resul < 1) {
+                    $respuesta['correcto'] = 'N';
+                    $respuesta['tipoError'] = 'errorGuardar';
+                    $respuesta['msj'] = utf8_encode('Error al guardar.');
+
+                }
+            } else { //modificacion
+                if (isset($datos['pass']) && $datos['pass'] != '') $datos['actualizarPass'] = 'S';
+
+                $resul = $this->modelo->updateUsuario($datos);
+                if ($resul != 1) {
+                    $respuesta['correcto'] = 'N';
+                    $respuesta['tipoError'] = 'errorModificar';
+                    $respuesta['msj'] = utf8_encode('Error al modificar.');
+
+                }
+            }
+        }
+
+        echo json_encode($respuesta);
+    }
+
+    public function activarDesactivar($datos)
+    {
+        $respuesta['correcto'] = 'S';
+        $respuesta['tipoError'] = '';
+        if ($datos['activo'] == 'S') {
+            $respuesta['msj'] = utf8_encode('Se a activado el usuario.');
+        } else {
+            $respuesta['msj'] = utf8_encode('Se desactivado el usuario.');
+        }
+        $resul = $this->modelo->activarDesactivarUsuario($datos);
+        if ($resul != 1) {
+            $respuesta['correcto'] = 'N';
+            $respuesta['tipoError'] = 'errorModificar';
+            $respuesta['msj'] = utf8_encode('Error al cambiar el estado del usuario.');
+
+        }
+        echo json_encode($respuesta);
+    }
 }
--- index.php
+++ index.php
@@ -1,39 +1,113 @@
 <?php session_start();
-	require_once 'config.php';
-	$msj='';
-	if( isset($_POST['login']) && isset($_POST['pass'])){
-		
-		require_once 'controladores/UsuariosC.php';
-		$usuario=new UsuariosC();
-		$res=$usuario->validarUsuario(array('login'=>$_POST['login'],
-											'pass'=>$_POST['pass']));
-		if( $res['valido']=='S'){
-			$_SESSION['login']=$_POST['login'];
-			header('location: app.php');
-		}else{
-			$msj=$res['msj'];
-		}
-	}
+require_once 'config.php';
+$msj = '';
+if (isset($_POST['login']) && isset($_POST['pass'])) {
+
+    require_once 'controladores/UsuariosC.php';
+    $usuario = new UsuariosC();
+    $res = $usuario->validarUsuario(array('login' => $_POST['login'],
+        'pass' => $_POST['pass']));
+    if ($res['valido'] == 'S') {
+        $_SESSION['login'] = $_POST['login'];
+        header('location: app.php');
+    } else {
+        $msj = $res['msj'];
+    }
+}
 ?>
 <!DOCTYPE HTML>
 <html lang="es">
-	<head>
-		<title>Gestion de empresa</title>
-	</head>
-	<body>
-		<span style="color:blue;"><?php echo $msj;?></span>
-		<form action="index.php" method="post" class="formulario">
-			<label for="login">Nombre:<br>
-				<input name="login"  id="login"	value="" size="40">
-			</label><br><br>
-			<label for="pass">Contrase&ntilde;a:<br> 
-				<input name="pass" id="pass" size="12" value="" 
-			 		type="password">
-			</label> <br><br>
-			<button type="submit">Entrar</button>
-		</form>
+<head>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <meta charset="UTF-8">
 
-		<h4>Nombre: admin</h4>
-		<h4>Contraseña: 123</h4>
-	</body>
-</html>	
+    <title>Gestion de empresa</title>
+    <style type="text/css">
+        body {
+            background: url('images/Eor57Ae.jpg') no-repeat fixed center center;
+            background-size: cover;
+            font-family: Montserrat;
+        }
+
+        .divPrincipal {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            margin: -100px 0 0 -150px;
+        }
+
+        .login-block {
+            width: 320px;
+            padding: 20px;
+            background: #fff;
+            border-radius: 5px;
+            border-top: 5px solid #ff656c;
+            margin: 0 auto;
+        }
+
+        .login-block h1 {
+            text-align: center;
+            color: #000;
+            font-size: 18px;
+            text-transform: uppercase;
+            margin-top: 0;
+            margin-bottom: 20px;
+        }
+
+        .login-block input {
+            width: 100%;
+            height: 42px;
+            box-sizing: border-box;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+            margin-bottom: 20px;
+            font-size: 14px;
+            font-family: Montserrat;
+            padding: 0 20px 0 50px;
+            outline: none;
+        }
+
+        .login-block input:active, .login-block input:focus {
+            border: 1px solid #ff656c;
+        }
+
+        .login-block button {
+            width: 100%;
+            height: 40px;
+            background: #ff656c;
+            box-sizing: border-box;
+            border-radius: 5px;
+            border: 1px solid #e15960;
+            color: #fff;
+            font-weight: bold;
+            text-transform: uppercase;
+            font-size: 14px;
+            font-family: Montserrat;
+            outline: none;
+            cursor: pointer;
+        }
+
+        .login-block button:hover {
+            background: #ff7b81;
+        }
+    </style>
+</head>
+<body>
+<div class="divPrincipal">
+    <form action="index.php" method="post" class="formulario">
+        <div class="login-block">
+            <label>Iniciar sesion</label><br><br>
+            <input type="text" placeholder="Usuario" id="login" name="login"/>
+            <input type="password" placeholder="Contraseña" id="pass" name="pass"/>
+            <button type="submit">Entrar</button>
+            <center><span style="color:blue;"><?php echo $msj; echo "<br>"; echo 'Acceso para clase y pruebas:'; echo "<br>"; echo 'Usuario: <strong>admin</strong>'; echo '<br>'; echo 'Contraseña: <strong>123</strong>'; ?></span></center>
+        </div>
+    </form>
+</div>
+<footer>
+    <div style="position: fixed; bottom: 0; background-color: #eaeaea; width: 100%;">
+        <center>© David Suescun Pelegay - 2º SI - Desarrollo de Interfaces - CES San Valero</center>
+    </div>
+</footer>
+</body>
+</html>
